71e95dad33b44bbcb947b7902f4faabcbd2c1777
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-05-17 12:25:33 -0500

    [ADD] Manifest

--- discount_product/__manifest__.py
+++ discount_product/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "Descuento a productos",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Descuento a productos""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Assetel",
+    'website': "http://www.assetel.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
@@ -20,16 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale_management'],
 
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
 }
