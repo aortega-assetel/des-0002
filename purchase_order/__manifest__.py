83cfbc4a823bed19527807c47143e124479b4248
memoalvarez <51715427+memoalvarez@users.noreply.github.com>
2021-05-17 16:44:25 -0500

    [TEST]

--- purchase_order/__manifest__.py
+++ purchase_order/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale_management', 'account'],
+    'depends': ['base', 'sale_management', 'account', 'account_accountant'],
 
     # always loaded
     'data': [
