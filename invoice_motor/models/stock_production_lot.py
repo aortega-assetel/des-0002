5d07064f72d7359f357baedef193624a9df62743
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-05-18 09:39:51 -0500

    [fix] delete last changes

--- invoice_motor/models/stock_production_lot.py
+++ invoice_motor/models/stock_production_lot.py
@@ -11,17 +11,7 @@ class StockProductionLot(models.Model):
     motor = fields.Boolean(string='Motor', default=False)
     no_motor = fields.Char(string='No. Motor')
 
-    @api.model
-    def create(self, vals):
-        result = super(StockProductionLot, self).create(vals)
 
-        serie = self.env['stock.move.line'].search([('lot_id.name', '=', result.name)])
-        _logger.info(str(serie))
-        _logger.info(str(vals))
-        result.motor = True
-        result.no_motor = vals.no_motor
-
-        return result
 
 
     
