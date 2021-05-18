e8036b8aeff46d19066453b6762d3133437d7a85
Emilio Curiel <58865901+emilio21-assetel@users.noreply.github.com>
2021-05-18 12:39:50 -0500

    [FIX]Se corrigio create

--- credit_request/models/project_task.py
+++ credit_request/models/project_task.py
@@ -17,29 +17,6 @@ class ProjectTask(models.Model):
 
         if result.request_task:
             result.name = '(#' + str(result.id) + ') - ' + result.name
-
-            text = '''<div style="margin: 0px; padding: 0px;">
-                <p style="margin: 0px; padding: 0px; font-size: 13px;">
-                    Buen día,
-                    <br/><br/>
-                    Se ha creado una nueva tarea de solicitudes de crédito:
-                    <br/><br/>
-                    Proyecto: ''' + str(result.project_id.name) + '''
-                    <br/>
-                    Nombre: ''' + str(result.name) + '''
-                    <br/>
-                    Oportunidad: ''' + str(result.lead_id.name) + '''
-                    <br/>
-                    Cliente: ''' + str(result.partner_id.name) + '''
-                    <br/>
-                    Descripción: ''' + str(result.description) + '''
-                    <br/><br/>
-                    Saludos.
-                </p>'''
-
-            for line in result.project_id.users_to_notify:
-                result.message_post(body=text, partner_ids=[line.partner_id.id])
-        
         return result
 
     def action_view_lead(self):
