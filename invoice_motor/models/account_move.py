3d35fef8d89ee8e04ed388a2e1a3d0e717ab684d
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-05-18 15:54:47 -0500

    a

--- invoice_motor/models/account_move.py
+++ invoice_motor/models/account_move.py
@@ -10,22 +10,34 @@ class AccountMove(models.Model):
         result = super(AccountMove, self).create(vals)
         count = 0
         for line in result.invoice_line_ids:
-            for i in range(line.quantity):
-                line.sequence = count
-                note_text = ''
+            line.sequence = count
+            note_text = ''
+            
+
+            for line_sale in line.sale_line_ids:
                 for product_variant in line.product_id.product_template_attribute_value_ids:
                     if 'Modelo' in product_variant.display_name or 'Marca' in product_variant.display_name or 'Cilindrada' in product_variant.display_name or 'Color' in product_variant.display_name:
                         attribute = product_variant.display_name
                         note_text = note_text + attribute + '.\n'
 
-                values = {
-                        'name' : note_text,
-                        'display_type': 'line_note',
-                        'move_id': result.id,
-                        'sequence': count + 1,
-                    }
-                new_note = self.env['account.move.line'].create(values)
-                count = count + 2
+                for line_move in line_sale.move_ids:
+                    stock_move_line = self.env['stock.move.line'].search([('move_id', '=', line_move.id)])
+
+                    for linea in stock_move_line:
+                        serie = linea.lot_id.name
+                        no_motor = linea.lot_id.no_motor
+
+                        note_text = note_text + 'Serie: ' + serie +'.\n' + 'No Motor: ' + no_motor +'.\n'
+            
+
+            values = {
+                    'name' : note_text,
+                    'display_type': 'line_note',
+                    'move_id': result.id,
+                    'sequence': count + 1,
+                }
+            new_note = self.env['account.move.line'].create(values)
+            count = count + 2
 
 
         return result
