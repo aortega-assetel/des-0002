db787c8c888a87a4b57aa17c409af680bc535d6f
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-05-18 12:21:36 -0500

    [ADD] note values

--- invoice_motor/models/account_move.py
+++ invoice_motor/models/account_move.py
@@ -8,13 +8,27 @@ class AccountMove(models.Model):
     @api.model
     def create(self, vals):
         result = super(AccountMove, self).create(vals)
-
-        values = {
-                    'name' : 'Hola este es un texto jshdfsdf',
-                    'display_type': 'line_note',
-                    'move_id': result.id,
-                }
-        new_note = self.env['account.move.line'].create(values)  
+        count = 0
+        for line in result.invoice_line_ids:
+            line.sequence = count
+            product = self.env['product.template'].search([('id', '=', line.product_id.id)])
+            note_text = ''
+            attribute_value = ''
+            for product_variant in product.attribute_line_ids:
+                for attribute_line in product_variant.attribute_id.value_ids:
+                    attribute = product_variant.attribute_id.name
+                    attribute_value = attribute_value + ', ' + attribute_line.name
+                
+                
+                note_text = note_text + attribute + ': ' + attribute_value + '.\n'
+            values = {
+                        'name' : note_text,
+                        'display_type': 'line_note',
+                        'move_id': result.id,
+                        'sequence': count + 1,
+                    }
+            new_note = self.env['account.move.line'].create(values)
+            count = count + 2
 
         return result
     
