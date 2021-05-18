15c040ce83486a422fc48221bdc109f189959583
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-05-18 13:24:10 -0500

    [fix] error

--- invoice_motor/models/account_move.py
+++ invoice_motor/models/account_move.py
@@ -41,7 +41,7 @@ class AccountMove(models.Model):
             line.sequence = count
             note_text = ''
             for product_variant in line.product_id.product_template_attribute_value_ids:
-                if product_variant.name == 'Modelo' or product_variant.name == 'Marca' or product_variant.name == 'Cilindrada' or product_variant.name == 'Color':
+                if 'Modelo' in product_variant.display_name or 'Marca' in product_variant.display_name or 'Cilindrada' in product_variant.display_name or 'Color' in product_variant.display_name:
                     attribute = product_variant.display_name
                     note_text = note_text + attribute + '.\n'
 
