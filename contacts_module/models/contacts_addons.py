cd5dd9303a9c45e1244b67f124470b770ff11e3b
memoalvarez <51715427+memoalvarez@users.noreply.github.com>
2021-05-19 15:51:58 -0500

    [TEST] Concatenando campos 1

--- contacts_module/models/contacts_addons.py
+++ contacts_module/models/contacts_addons.py
@@ -4,7 +4,17 @@ from odoo import models, fields, api
 
 class ContactsAddons(models.Model):
     _inherit = 'res.partner'
+    
+    @api.depends('name_indiviual', 'surname_matern', 'surname_patern')
+    def _concatenate_fields_contacts_addons(self):
+        for reg in self:
+            nombre = reg.name_indiviual + reg.surname_matern + reg.surname_patern
 
-    name_indiviual = fields.Char(string="Nombre(s)")
+        reg.update({
+            'name': nombre
+        })
+
+
+    name_indiviual = fields.Char(string="Nombre(s)", store=True, compute='_concatenate_fields_contacts_addons', tracking=4)
     surname_matern = fields.Char(string="Apellido materno")
     surname_patern = fields.Char(string="Apellido paterno")
