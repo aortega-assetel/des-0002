ad53ad2d1828b3222cf3ac675ca76f933c5c6439
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-05-19 17:00:57 -0500

    [fix]1

--- accounting_entry/models/mrp_workcenter.py
+++ accounting_entry/models/mrp_workcenter.py
@@ -2,6 +2,7 @@
 from typing import Sequence
 from odoo import models, api, fields
 import logging
+from datetime import datetime
 
 _logger = logging.getLogger(__name__)
 
@@ -17,28 +18,27 @@ class MrpWorkcenter(models.Model):
         result = super(MrpProduction, self).button_mark_done()
         if self.state == 'done':
             for line in self.workorder_ids:
-                
-                if line.product_id.categ_id.property_valuation == 'real_time':
-                    move_lines = [
-                                (0, 0, {
-                                    'account_id' : line.workcenter_id.cuenta_abono.id,
-                                    'name': self.name +  ' - Costo de producción',
-                                    'credit': line.duration * (line.workcenter_id.costs_hour/60),
-                                    
-                                }),
-                                (0, 0, {
-                                    'account_id' : line.workcenter_id.cuenta_cargo.id,
-                                    'name': self.name +  ' - Costo de producción',
-                                    'debit': line.duration * (line.workcenter_id.costs_hour/60),
-                                })
-                            ]
-                    values = {
-                        'ref' : self.name +  ' - ' + line.product_id.name,
-                        'date' : self.write_date,
-                        'journal_id' : line.workcenter_id.journal.id,
-                        'line_ids' : move_lines
-                        }
-                    asiento = self.env['account.move'].create(values)
-                    asiento.action_post()
+                cost = line.duration * (line.workcenter_id.costs_hour/60)
+                move_lines = [
+                            (0, 0, {
+                                'account_id' : line.workcenter_id.cuenta_abono.id,
+                                'name': self.name +  ' - Costo de producción',
+                                'credit': cost,
+                                
+                            }),
+                            (0, 0, {
+                                'account_id' : line.workcenter_id.cuenta_cargo.id,
+                                'name': self.name +  ' - Costo de producción',
+                                'debit': cost,
+                            })
+                        ]
+                values = {
+                    'ref' : self.name +  ' - Costo de producción',
+                    'date' : datetime.today().strftime('%d/%m/%Y'),
+                    'journal_id' : line.workcenter_id.journal.id,
+                    'line_ids' : move_lines
+                    }
+                asiento = self.env['account.move'].create(values)
+                asiento.action_post()
             
         return result
