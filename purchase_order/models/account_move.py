002796cfe55c2178e3416bbabf3526dda95ecbb9
memoalvarez <51715427+memoalvarez@users.noreply.github.com>
2021-05-20 13:56:53 -0500

    [TEST] 6

--- purchase_order/models/account_move.py
+++ purchase_order/models/account_move.py
@@ -16,9 +16,8 @@ class AccountMove(models.Model):
     def create(self, vals):
         result = super(AccountMove, self).create(vals)
         sale_order = self.env['sale.order'].search([('name', '=', result.invoice_origin)])
-        _logger.info(str(sale_order[0]))
         _logger.info(str(sale_order.purchase_order))
-        self.purchase_order = str(sale_order.purchase_order)
+        result.purchase_order = str(sale_order.purchase_order)
         
 
 
