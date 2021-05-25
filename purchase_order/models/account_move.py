7e3ea80a161fe56e5115cdbb6ae304d928a93731
memoalvarez <51715427+memoalvarez@users.noreply.github.com>
2021-05-25 16:10:06 -0500

    [TEST] 4

--- purchase_order/models/account_move.py
+++ purchase_order/models/account_move.py
@@ -1,24 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-import logging
-
-_logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     purchase_order = fields.Char(string='Orden de compra')
 
-
-
     @api.model
     def create(self, vals):
         result = super(AccountMove, self).create(vals)
         sale_order = self.env['sale.order'].search([('name', '=', result.invoice_origin)])
-        _logger.info(str(sale_order.purchase_order))
-        result.purchase_order = str(sale_order.purchase_order)
+        if sale_order.purchase_order:
+            result.purchase_order = str(sale_order.purchase_order)
         
-
-
         return result
