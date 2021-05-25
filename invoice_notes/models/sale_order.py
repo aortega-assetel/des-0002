a2bbe691180e02789b18c8f347662be10960120f
Emilio Curiel <58865901+emilio21-assetel@users.noreply.github.com>
2021-05-25 13:56:23 -0500

    [TEST]Prueba nota 1

--- invoice_notes/models/sale_order.py
+++ invoice_notes/models/sale_order.py
@@ -10,11 +10,12 @@ class SaleOrder(models.Model):
     def invoice_note(self, vals):
         for bud in self:
             for fac in bud.account_move_id:
-                budget_id = self.env['sale.order'].search([('name', '=', fac.invoice_origin)])
-                if budget_id:
-                    for mess in bud.picking_ids:
-                        if bud.invoice_count > 0:
-                            mess.message_post(body='PEDIDO FACTURADO <br/><br/><button name="%(action_view_url)d" string="FACTURAR" type="action"/>')
+                sale_order = self.env['sale.order'].search([])
+                for sl_order in sale_order:
+                    if sl_order.name == fac.invoice_origin:
+                        if fac.state == 'posted':
+                            for mess in bud.picking_ids:
+                                mess.message_post(body='PEDIDO FACTURADO <br/><br/><button name="%(action_view_url)d" string="FACTURAR" type="action"/>')
 
 
     def action_view_url(self):
