a2b5a979f99eb6844985378e2b6aa0faf88bf441
Emilio Curiel <58865901+emilio21-assetel@users.noreply.github.com>
2021-05-26 14:43:46 -0500

    [TEST]Prueba activity 1

--- invoice_notes/models/account_move.py
+++ invoice_notes/models/account_move.py
@@ -9,28 +9,38 @@ class AccountMove(models.Model):
     sale_order_id = fields.Many2one('sale.order', string="Facturas")
     delivery_id = fields.Many2one('stock.picking', string='Pedidos')
 
+
     def action_post(self):
         result = super(AccountMove, self).action_post()
-        body = '<p>PEDIDO FACTURADO:</p><p style=" "></p><a href="/web#id='+ str(self.id) +'&amp;action=270&amp;active_id=21&amp;model=account.move&amp;view_type=form&amp;cids=1&amp;menu_id=233" class="btn btn-primary" style=" ">Ir a Factura</a><p></p>'
+        body = '<p>PEDIDO FACTURADO:</p><p style=" "></p><a href="/web#id='+ str(self.id) +'&amp;action=270&amp;active_id=21&amp;',
+        'model=account.move&amp;view_type=form&amp;cids=1&amp;menu_id=233" class="btn btn-primary" style=" ">Ir a Factura</a><p></p>'
         if self.state == 'posted':
             sale_order = self.env['sale.order'].search([('name', '=', self.invoice_origin)])
             for mess in sale_order.picking_ids:
                 mess.message_post(body=body)
+                if mess.stock_activity_id:
+                    mess.stock_activity_id.unlink()
+                model_id = self.env['ir.model']._get(self._name).id
+                activity_id = self.env['mail.activity.type'].search([['demo_ending_activity', '=', True]])
+
         return result
 
 
-    def action_view_url(self):
-        id_view = self.id
-        for reg in self.sale_order_id:
-            if reg.invoice_count>1:
-                return {
-                    'type': 'ir.actions.act_url',
-                    'url': "https://aortega-assetel-des-0002-test1-2563447.dev.odoo.com/web?#id=%s&action=270&active_id=4&model=account.move&view_type=list&cids=1&menu_id=233" % id_view,
-                    'target': 'new',
-                }
-            else:
-                return {
-                    'type': 'ir.actions.act_url',
-                    'url': "https://aortega-assetel-des-0002-test1-2563447.dev.odoo.com/web?#id=%s&action=270&active_id=4&model=account.move&view_type=form&cids=1&menu_id=233" % id_view,
-                    'target': 'new',
-                }
+    def create_activity_demo(self, result):
+        if result.stock_activity_id:
+            result.stock_activity_id.unlink()
+
+        model_id = self.env['ir.model']._get(self._name).id
+        activity_id = self.env['mail.activity.type'].search([['demo_ending_activity', '=', True]])
+
+        vals = {
+            'res_model' : "installed.services",
+            'res_model_id' : model_id,
+            'res_id' : result.id,
+            'summary' : "Finalizar demo",
+            'activity_type_id' : activity_id.id,
+            'date_deadline' : result.demo_finish_date,
+            'user_id' : result.project_task.project_id.user_id.id,
+        }
+        new_activity = self.env['mail.activity'].create(vals)
+        result.demo_activity_id = new_activity.id
