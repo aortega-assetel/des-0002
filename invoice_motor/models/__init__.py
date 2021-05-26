0ba41bfc1b0fb5a4d69d624078e6482f79f13b91
Marco Antonio Navarro Camarena <marcoanc95@gmail.com>
2021-05-26 11:24:56 -0500

    6

--- invoice_motor/models/__init__.py
+++ invoice_motor/models/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from . import stock_production_lot
-from . import stock_move_line
 from . import account_move
 from . import stock_landed_cost
+from . import stock_move_line
+from . import stock_production_lot
+
+
--- invoice_motor/models/stock_move_line.py
+++ invoice_motor/models/stock_move_line.py
@@ -7,18 +7,5 @@ _logger = logging.getLogger(__name__)
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    no_motor = fields.Char(string='No. Motor')
-
-    @api.onchange('lot_id')
-    def _onchange_state(self):
-        #By Alan Ortega
-        _logger.info('MODEL: 1')
-        if self.lot_id:
-            _logger.info('MODEL: 3')
-            self.lot_id.motor = True
-            self.lot_id.no_motor = self.no_motor
-        #By Alan Ortega
-
-
-    
+    no_motor = fields.Char(string='No. Motor')    
         
