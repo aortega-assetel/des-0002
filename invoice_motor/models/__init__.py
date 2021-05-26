abbe6c0b5db610e7a9845a6a845cc6da1f505e50
Marco Antonio Navarro Camarena <marcoanc95@gmail.com>
2021-05-26 10:48:31 -0500

    3

--- invoice_motor/models/__init__.py
+++ invoice_motor/models/__init__.py
@@ -2,6 +2,5 @@
 
 from . import stock_production_lot
 from . import stock_move_line
-from . import stock_picking
 from . import account_move
 from . import stock_landed_cost
--- invoice_motor/models/stock_move_line.py
+++ invoice_motor/models/stock_move_line.py
@@ -6,6 +6,16 @@ class StockMoveLine(models.Model):
 
     no_motor = fields.Char(string='No. Motor')
 
+    @api.onchange('state')
+    def _onchange_state(self):
+        #By Alan Ortega
+        _logger.info('MODEL: 1')
+        if self.state == 'done':
+            _logger.info('MODEL: 3')
+            self.lot_id.motor = True
+            self.lot_id.no_motor = self.no_motor
+        #By Alan Ortega
+
 
     
         
