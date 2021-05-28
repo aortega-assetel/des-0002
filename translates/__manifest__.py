c7bc1f8423a01a3a8d48ee58b17977b5031dfe24
memoalvarez <51715427+memoalvarez@users.noreply.github.com>
2021-05-28 17:57:30 -0500

    [TEST] 1

---
+++ translates/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Traducciones",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Assetel",
+    'website': "http://www.assetel.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock', 'l10n_mx_edi_extended'],
+
+    # always loaded
+    'data': [
+        'views/product_template_translates.xml',
+    ],
+}
