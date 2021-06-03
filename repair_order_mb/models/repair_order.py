8cbd02873ddc9ad7857831f93dfc8b606539e3fb
aortega-assetel <80126348+aortega-assetel@users.noreply.github.com>
2021-06-03 13:01:57 -0500

    3

--- repair_order_mb/models/repair_order.py
+++ repair_order_mb/models/repair_order.py
@@ -4,13 +4,14 @@ from odoo import models, api, fields
 class RepairOrder(models.Model):
     _inherit = 'repair.order'
 
-    lot_id = fields.Many2one(
+    lot_id_mb = fields.Many2one(
         'stock.production.lot', 'Lot/Serial', check_company=True,
         help="Products repaired are all belonging to this lot")
 
 
-    @api.onchange('lot_id')
+    @api.onchange('lot_id_mb')
     def _onchange_lot_id(self):
-        if self.lot_id:
+        if self.lot_id_mb:
             serie = self.env['stock.production.lot'].search([['id', '=', self.lot_id]])
             self.product_id = serie.product_id
+            self.lot_id = self.lot_id_mb
