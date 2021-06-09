4210028b0585a8492a9e1b0624e7cb856c609375
Emilio Curiel <58865901+emilio21-assetel@users.noreply.github.com>
2021-06-09 11:05:24 -0500

    [TEST]Prueba posicion campos PDF 1

--- warranty_card/models/warranty_card.py
+++ warranty_card/models/warranty_card.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, SUPERUSER_ID, api, _ 
+from odoo import models, fields, SUPERUSER_ID, api, datetime, DEFAULT_SERVER_DATE_FORMAT,  _ 
 
 class WarrantyCard(models.Model):
     _name = 'warranty.card'
@@ -33,6 +33,10 @@ class WarrantyCard(models.Model):
     #no_serie = fields.Many2one('stock.production.lot', string='N° de Serie')
     no_serie = fields.Char(string='N° de Serie')
     fecha_entrega = fields.Date(string='Fecha de Entrega')
+    date = datetime.strptime(fecha_entrega, DEFAULT_SERVER_DATE_FORMAT)
+    day = date.day
+    month = date.month
+    year = date.year
     no_motor = fields.Char(string='No de Motor')
     modelo = fields.Char(string='Modelo')
     #no_motor = fields.Many2one('res.partner', string='No de Motor', related='requests_id.no_motor')
