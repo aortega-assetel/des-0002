2cdb877d0c19cb3bdb9e4ecc920c0bc36753c43d
Emilio Curiel <58865901+emilio21-assetel@users.noreply.github.com>
2021-06-14 11:58:13 -0500

    [TEST]Prueba posicion campos PDF 28

--- warranty_card/__manifest__.py
+++ warranty_card/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'contacts', 'stock', 'report'], 
+    'depends': ['base', 'contacts', 'stock'], 
 
     # always loaded
     'data': [
